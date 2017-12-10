eb42cb1d396156bad54286f5b921e7c583ea4e50
Yusingwang <tlsrhkd5712@naver.com>
2017-12-10 14:47:32 +0900

    이벤트 뷰 글쓰기x

--- EveryCVS/EveryCVS_projectFolder/everycvs/src/main/java/com/kh/everycvs/event/controller/EventController.java
+++ EveryCVS/EveryCVS_projectFolder/everycvs/src/main/java/com/kh/everycvs/event/controller/EventController.java
@@ -2,10 +2,14 @@ package com.kh.everycvs.event.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.everycvs.common.model.vo.Event;
 import com.kh.everycvs.event.model.service.EventService;
 
 @Controller
@@ -15,31 +19,75 @@ public class EventController {
 	private EventService eventService;
 	
 	// 이벤트 조회 : 모든 공식이벤트를 조회
-	public ModelAndView selectEventList(HttpServletRequest request) {
-		return null;
+	@RequestMapping(value="eventAll.do", method=RequestMethod.GET)
+	public String selectEventList(HttpServletRequest request) {
+		return "event/eventlist";
 	}
 	
+	
 	// 이벤트 조회 : 선택한 이벤트 상세조회
+	@RequestMapping(value="eventDetail.do", method=RequestMethod.GET)
 	public ModelAndView selectEventOne(HttpServletRequest request) {
 		return null;
 	}
 	
-	// 이벤트 검색 : 제목으로 검색
+	// 업체명 검색
+	@RequestMapping(value="eventSearch.do", method=RequestMethod.GET)
 	public ModelAndView searchEventList(HttpServletRequest request) {
 		return null;
 	}
 	
-	// 이벤트 등록
-	public String insertEvent(HttpServletRequest request) {
-		return null;
+	
+	
+	
+	
+	
+	
+	
+	/*글작성 시작 -----------------------------------------------------*/
+	//글쓰기 페이지 이동
+	 @RequestMapping(value ="eventInsert.do", method = RequestMethod.GET)
+     public String eventInsert() {
+        return "event/eventWhirtForm";
+     }
+	 
+	// 글쓰기 페이지 저장
+	@RequestMapping(value="eventInsert.do", method=RequestMethod.POST)
+	public String insertEvent(Event eventvo) {
+		  System.out.println(eventvo);
+          int result = eventService.eventInsert(eventvo);
+          //1이상 result 값이면 리스트로이동 
+             return "redirect:/eventAll.do";
+          
 	}
+	/*글작성 끝 --------------------------------------------------------*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	// 이벤트 수정
+	@RequestMapping(value="eventUpdate.do", method=RequestMethod.GET)
 	public String updateEvent(HttpServletRequest request) {
 		return null;
 	}
 	
 	// 이벤트 삭제
+	@RequestMapping(value="eventDelete.do", method=RequestMethod.GET)
 	public String deleteEvent(HttpServletRequest request) {
 		return null;
 	}
